0e621c3abc9ab9d56ecb30c32696c5e171252a42
scott <scott>
1999-11-01 04:35:46 +0000

    Moved uncompr.c to src/zlib

--- src/id3/uncompr.c
+++ src/id3/uncompr.c
@@ -3,7 +3,7 @@
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
-/* @(#) $Id: uncompr.c,v 1.1 1999/11/01 03:14:30 scott Exp $ */
+/* @(#) $Id: uncompr.c,v 1.1 1999/11/01 04:35:46 scott Exp $ */
 
 #include "zlib.h"
 
