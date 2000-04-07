1e775bfb81e527cb8aed2a4867bc92b3b19f399c
eldamitri <eldamitri>
2000-04-07 19:35:14 +0000

    Added HasV1Tag, HasV2Tag, and HasLyrics (thanks John Adcock).

--- include/id3/tag.h
+++ include/id3/tag.h
@@ -1,4 +1,4 @@
-// $Id: tag.h,v 1.9 2000/04/07 04:45:31 eldamitri Exp $
+// $Id: tag.h,v 1.10 2000/04/07 19:35:14 eldamitri Exp $
 
 // id3lib: a C++ library for creating and manipulating id3v1/v2 tags
 // Copyright 1999, 2000  Scott Thomas Haug
@@ -93,7 +93,7 @@ const luint LEN_V1_GENRE   =   1;
 
     @author Dirk Mahoney (dirk@id3.org)
     @author Scott Thomas Haug (sth2@cs.wustl.edu)
-    @version $Id: tag.h,v 1.9 2000/04/07 04:45:31 eldamitri Exp $
+    @version $Id: tag.h,v 1.10 2000/04/07 19:35:14 eldamitri Exp $
     @see ID3_Tag
 */
 struct ID3V1_Tag
@@ -192,7 +192,7 @@ const luint ALL_TAG_TYPES = BOTH_ID3_TAGS | LYRICS_TAG;
      that id3lib 2.16 supports.
 
      @author Dirk Mahoney
-     @version $Id: tag.h,v 1.9 2000/04/07 04:45:31 eldamitri Exp $
+     @version $Id: tag.h,v 1.10 2000/04/07 19:35:14 eldamitri Exp $
      @see ID3_Frame
      @see ID3_Field
      @see ID3_Err
@@ -774,6 +774,37 @@ public:
   
   ID3_Tag &operator=( const ID3_Tag &rTag );
 
+  /** Indicates whether the an V1 tag is present.
+     
+      <p>
+       
+      This method was inserted for use by the ID3Com .dll.
+     
+      @return Whether or not a V1 tag is present.
+  */
+  bool HasV1Tag() {return __bHasV1Tag;};
+
+  /** Indicates whether the an V2 tag is present.
+
+      <p>
+     
+      This method was inserted for use by the ID3Com .dll.
+     
+      @return Whether or not a V2 tag is present.
+  */
+  bool HasV2Tag() {return (__ulOldTagSize > 0);};
+     
+  /** Indicates whether there are Lyrics present.
+     
+      <p>
+     
+      This method was inserted for use by the ID3Com .dll.
+
+      @return Whether or not Lyrics are present.
+  */
+  bool HasLyrics() {return __bHasV1Tag;};
+
+
 protected:
   void      AddFrame(ID3_Frame *pNewFrame, bool bFreeWhenDone);
   void      AddFrames(ID3_Frame *newFrames, luint nFrames, bool freeWhenDone);
@@ -829,6 +860,9 @@ private:
 #endif
 
 // $Log: tag.h,v $
+// Revision 1.10  2000/04/07 19:35:14  eldamitri
+// Added HasV1Tag, HasV2Tag, and HasLyrics (thanks John Adcock).
+//
 // Revision 1.9  2000/04/07 04:45:31  eldamitri
 // Minor cleanup for log comments.
 //
