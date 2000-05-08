be827dfc4a281ad1cf1355358575223d05d9e70a
eldamitri <eldamitri>
2000-05-08 03:06:12 +0000

    (Find): All are now const methods

--- src/tag_find.cpp
+++ src/tag_find.cpp
@@ -1,4 +1,4 @@
-// $Id: tag_find.cpp,v 1.8 2000/05/06 21:16:45 eldamitri Exp $
+// $Id: tag_find.cpp,v 1.9 2000/05/08 03:06:12 eldamitri Exp $
 
 // id3lib: a C++ library for creating and manipulating id3v1/v2 tags
 // Copyright 1999, 2000  Scott Thomas Haug
@@ -111,7 +111,7 @@ ID3_Elem *ID3_Tag::Find(ID3_Frame *frame) const
   return elem;
 }
 
-ID3_Frame *ID3_Tag::Find(ID3_FrameID id)
+ID3_Frame *ID3_Tag::Find(ID3_FrameID id) const
 {
   ID3_Frame *frame = NULL;
   
@@ -148,7 +148,7 @@ ID3_Frame *ID3_Tag::Find(ID3_FrameID id)
   return frame;
 }
 
-ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, char *data)
+ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, char *data) const
 {
   ID3_Frame *frame = NULL;
   unicode_t *temp;
@@ -166,7 +166,7 @@ ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, char *data)
   return frame;
 }
 
-ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, unicode_t *data)
+ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, unicode_t *data) const
 {
   ID3_Frame *frame = NULL;
   
@@ -218,7 +218,7 @@ ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, unicode_t *data)
   return frame;
 }
 
-ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, luint data)
+ID3_Frame *ID3_Tag::Find(ID3_FrameID id, ID3_FieldID fld, luint data) const
 {
   ID3_Frame *frame = NULL;
   
